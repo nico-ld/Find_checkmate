419b3fdb570b09fcf633bd64075b9250d337295b
nico-ld <nico27ld@gmail.com>
2025-11-01 18:11:47 +0100

    checkmate.h main.c and ft_display.c created. Need to create a beautiful chessboard

---
+++ checkmate.h
@@ -0,0 +1,22 @@
+/* *********************************************** *\
+   /\   /\
+   \ \ / /			File Name : checkmate.h 
+    \ V /	
+    /. .\			Author : nico-ld
+   =\ T /=
+    / ^ \			Creation date : 01-11-2025 17:40 
+   /\\ //\
+ __\ " " /__		Last update : 01-11-2025 18:03 
+(____/^\____)
+\* *********************************************** */
+
+#ifndef CHEKMATE_H
+# define CHEKMATE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void	ft_display_board(char *chessboard);
+
+#endif
