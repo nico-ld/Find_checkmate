630adf62845836fdf9ca5d9a8f7c9db867a3e6e8
nico-ld <nico27ld@gmail.com>
2025-11-02 12:58:00 +0100

    Function to display the chessboard complete

--- ft_display.c
+++ ft_display.c
@@ -4,20 +4,52 @@
    ,----(     ..    )		Author : nico-ld
   /      \__     __/	
  /|         (\  |(			Creation date :	01-11-2025 17:52 
-^ \	  /___\  /\ |			Last update : 01-11-2025 18:09                 
+^ \	  /___\  /\ |			Last update : 02-11-2025 12:26                 
    |__|   |__|-''
 \* ==================================================== */
 
 #include "checkmate.h"
 
+/*
+		A B C D E F G H
+	   ________________
+	8 | R N B Q K B N R
+	7 | P P P P P P P P
+	6 | x x x x x x x x
+	5 | x x x x x x x x
+	4 | x x x x x x x x
+	3 | x x x x x x x x
+	2 | p p p p p p p p
+	1 | r n b q k b n r
+
+ */
+
+void	ft_putchar(char	c)
+{
+	write (1, &c, 1);
+}
+
 void	ft_display_board(char *chessboard)
 {
 	int	i;
+	int	row;
 
 	i = 0;
-	write (1, "________\n", 8);
-	while (i < 8)
+	row = 8;
+	write (1, "    A B C D E F G H\n", 20);
+	write (1, "   ________________\n", 20);
+	while (row > 0)
 	{
-		
+		ft_putchar(row + '0');
+		write (1, " |", 2);
+		row--;
+		while ((i + 1) % 8 != 0)
+		{
+			ft_putchar(' ');
+			ft_putchar(chessboard[i++]);
+		}
+		ft_putchar(' ');
+		ft_putchar(chessboard[i++]);
+		ft_putchar('\n');
 	}
 }
